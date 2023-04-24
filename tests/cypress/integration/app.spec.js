591d29b75750f5431fb2fa07f266b2d8a15561bd
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-04-24 19:00:11 +0200

    corrects all problems in app

--- tests/cypress/integration/app.spec.js
+++ tests/cypress/integration/app.spec.js
@@ -1,45 +1,48 @@
 describe('app', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
-  it('starts', () => {})
+  it('starts', () => {});
 
   it('Has more than 7 tabs', () => {
     cy
       .get('.nav.nav-pills a[data-bs-toggle=tab]')
-      .should('have.length.at.least', 7)
-  })
+      .should('have.length.at.least', 7);
+  });
 
   it('Navigates to all tabs', () => {
     cy
       .get('.nav.nav-pills a[data-bs-toggle=tab]')
-      .each(($el, index, $list) => {
-        cy
-          .wrap($el)
-          .click()
-          .invoke('attr', 'href')
-          .as("href_tab")
+      .each(($el) => {
+        cy.wrap($el).as('el');
 
-        cy.get('@href_tab').then(href_tab => {
-          cy
-            .get(`${href_tab}.tab-pane.active`).should('be.visible')
-            .then(tab_pane => {
-              if (tab_pane.find('.shiny-bound-output').length > 0) {
-                cy.wrap(tab_pane)
-                  .find('.shiny-bound-output')
-                  .filter(':visible')
-                  .each(($el2, index2, $list2)  => {
-                    cy
-                      .wrap($el2)
-                      .children()
-                      .should('have.length.gte', 1)
-                  })
-              } else {
-                cy.log('Didn\'t find bound output, skipping test')
-              }
-            })
-        })
-      })
-  })
-})
+        cy.get('@el').click();
+
+        cy.get('@el').invoke('attr', 'href').as('hrefTab');
+
+        cy
+          .get('@hrefTab')
+          .then((hrefTab) => {
+            cy
+              .get(`${hrefTab}.tab-pane.active`)
+              .should('be.visible')
+              .then((tabPane) => {
+                if (tabPane.find('.shiny-bound-output').length > 0) {
+                  cy.wrap(tabPane)
+                    .find('.shiny-bound-output')
+                    .filter(':visible')
+                    .each(($el2) => {
+                      cy
+                        .wrap($el2)
+                        .children()
+                        .should('have.length.gte', 1);
+                    });
+                } else {
+                  cy.log('Didn\'t find bound output, skipping test');
+                }
+              });
+          });
+      });
+  });
+});
--- tests/cypress/integration/filter.spec.js
+++ tests/cypress/integration/filter.spec.js
@@ -1,16 +1,18 @@
 describe('app', () => {
 
-  const ns_prefix = 'app-'
-  const ns = (id) => `#${ns_prefix}${id}`
-  const ns_teal = (id) => ns(`teal_wrapper-teal-main_ui-${id}`)
-
+  // Rhino applications have an 'app' prefix
+  const nsPrefix = 'app-';
+  // Support function to help generating namespaces
+  const ns = (id) => `#${nsPrefix}${id}`;
+  // Support function to help generating teal namespaces on main UI
+  const nsTeal = (id) => ns(`teal_wrapper-teal-main_ui-${id}`);
 
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/');
 
     cy
       .get('html')
-      .not('.shiny-busy')
+      .not('.shiny-busy');
 
     // Find the tab and click to navigate
     cy
@@ -18,45 +20,44 @@ describe('app', () => {
       .contains('KM plot for TTDE')
       .click()
       .invoke('attr', 'href')
-      .as('href_tab')
+      .as('href_tab');
 
     // Make sure the correct tab is selected
     cy.get('@href_tab').then(href_tab => {
-
       // Look for 'Data is loaded' element
-      cy.contains('Data loaded - App fully started up')
+      cy.contains('Data loaded - App fully started up');
 
       // Define an alias that references the current active tab
       cy
         .get(`${href_tab}.tab-pane.active`)
         .should('be.visible')
-        .as('active_tab')
+        .as('active_tab');
 
       // Define an alias that references the active filters
       cy
-        .get(ns_teal('filter_panel-filters_overview'))
-        .as('filter_summary')
+        .get(nsTeal('filter_panel-filters_overview'))
+        .as('filter_summary');
 
       // Define an alias that references the active variables
       cy
-        .get(ns_teal('filter_panel-filter_active_vars'))
-        .as('filter_variables')
+        .get(nsTeal('filter_panel-filter_active_vars'))
+        .as('filter_variables');
 
       // Define an alias that references the section that adds new variables to
       //  the filters
       cy
-        .get(ns_teal('filter_panel-filter_add_vars'))
+        .get(nsTeal('filter_panel-filter_add_vars'))
         .should('be.visible')
-        .as('add_filter')
+        .as('add_filter');
 
       // Make sure that html element does not have a class that indicates
       // that shiny is busy
       cy
         .get('html')
-        .not('.shiny-busy')
-    })
+        .not('.shiny-busy');
+    });
 
-  })
+  });
 
   //  Filter summary
   // ######################################
@@ -71,10 +72,10 @@ describe('app', () => {
             cy
               .wrap($el)
               .children()
-              .should('have.length.gte', 1)
-          })
-      })
-  })
+              .should('have.length.gte', 1);
+          });
+      });
+  });
 
   //  Filter variables
   // ######################################
@@ -84,30 +85,30 @@ describe('app', () => {
       .get('@filter_variables')
       .within(() => {
         cy
-          .get(ns_teal('filter_panel-filter_active_vars_contents'))
-          .should('have.length.gte', 1)
+          .get(nsTeal('filter_panel-filter_active_vars_contents'))
+          .should('have.length.gte', 1);
 
         cy
-          .get(ns_teal('filter_panel-ADSL_filter-filter-cards'))
+          .get(nsTeal('filter_panel-ADSL_filter-filter-cards'))
           .children()
-          .should('have.length', 0)
+          .should('have.length', 0);
 
         cy
-          .get(ns_teal('filter_panel-ADAS_filter-filter-cards'))
+          .get(nsTeal('filter_panel-ADAS_filter-filter-cards'))
           .children()
-          .should('have.length', 0)
+          .should('have.length', 0);
 
         cy
-          .get(ns_teal('filter_panel-ADTTE_filter-filter-cards'))
+          .get(nsTeal('filter_panel-ADTTE_filter-filter-cards'))
           .children()
-          .should('have.length', 0)
+          .should('have.length', 0);
 
         cy
-          .get(ns_teal('filter_panel-ADLB_filter-filter-cards'))
+          .get(nsTeal('filter_panel-ADLB_filter-filter-cards'))
           .children()
-          .should('have.length', 0)
-      })
-  })
+          .should('have.length', 0);
+      });
+  });
 
   //  Add variables to filter
   // ######################################
@@ -118,23 +119,23 @@ describe('app', () => {
      cy
       .get('@add_filter')
       .within(() => {
-          cy
-            .get('.shiny-input-container')
-            .each(($el, index, $list) => {
-              cy
-                .wrap($el)
-                .children()
-                .should('have.length.gte', 1)
-            })
-        })
-  })
+        cy
+          .get('.shiny-input-container')
+          .each(($el, index, $list) => {
+            cy
+              .wrap($el)
+              .children()
+              .should('have.length.gte', 1)
+          });
+      });
+  });
 
   //  Add variables to filter
   // ######################################
 
   it('Add filter', () => {
     const ns_info_table = (selector) =>
-      ns_teal('filter_panel-teal_filters_info-table') + ` ${selector}`
+      nsTeal('filter_panel-teal_filters_info-table') + ` ${selector}`;
 
     // Add Age filter (find and click on it)
     // ------------------------------------
@@ -145,24 +146,24 @@ describe('app', () => {
           .get('.shiny-input-container:first')
           .contains('Select variable to filter')
           .should('be.visible')
-          .click("top")
+          .click("top");
 
         cy
           .get('.dropdown-menu.open li')
           .contains('Age')
-          .click("top")
+          .click("top");
       })
 
     // Let shiny finish rendering
     cy
       .get('html')
-      .not('.shiny-busy')
+      .not('.shiny-busy');
 
     // Test if application has non-zero subjects
 
     cy
       .get(ns_info_table('table tbody tr:first td:last'))
-      .contains(/[1-9]+[0-9]*\/[1-9]+[0-9]*/)
+      .contains(/[1-9]+[0-9]*\/[1-9]+[0-9]*/);
 
     // Monitor how many times summary is recalculated vias shiny:recalculated
     // event
@@ -171,8 +172,8 @@ describe('app', () => {
       .within(() => {
         cy
           .get('.shiny-bound-output')
-          .invoke('on', 'shiny:recalculated', cy.stub().as('summary_change'))
-      })
+          .invoke('on', 'shiny:recalculated', cy.stub().as('summary_change'));
+      });
 
     // Move slider via cypress2 mouse drag
     // ------------------------------------
@@ -180,17 +181,17 @@ describe('app', () => {
       .get('@filter_variables')
       .within(() => {
 
-        const dataTransfer = new DataTransfer()
+        const dataTransfer = new DataTransfer();
 
         // Make sure that the overlay plot is rendered
         cy
           .get('.filterPlotOverlayRange .shiny-plot-output')
           .children()
-          .should('have.length.gte', 1)
+          .should('have.length.gte', 1);
 
         cy
           .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating')
+          .not('.recalculating');
 
         // Move the handle 3x
         //  note: this is necessary as developer wasn't able to define a single
@@ -199,39 +200,39 @@ describe('app', () => {
           .get('.irs-handle.from')
           .trigger('mousedown', 'top', { button: 0, which: 1, pageX: 600, pageY: 100 })
           .trigger('mousemove', 'top', { clientX: 300, clientY: 400 })
-          .trigger('mouseup', 'top')
+          .trigger('mouseup', 'top');
 
         cy
           .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating')
+          .not('.recalculating');
         // cy.get('@summary_change', { timeout: 20000 }).should('have.callCount', 1)
 
         cy
           .get('.irs-handle.from')
           .trigger('mousedown', 'top', { button: 0, which: 1, pageX: 600, pageY: 100 })
           .trigger('mousemove', 'top', { clientX: 300, clientY: 400 })
-          .trigger('mouseup', 'top')
+          .trigger('mouseup', 'top');
 
         cy
           .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating')
+          .not('.recalculating');
 
         cy
           .get('.irs-handle.from')
           .trigger('mousedown', 'top', { button: 0, which: 1, pageX: 600, pageY: 100 })
           .trigger('mousemove', 'top', { clientX: 300, clientY: 400 })
-          .trigger('mouseup', 'top')
+          .trigger('mouseup', 'top');
 
         cy
           .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating')
-      })
+          .not('.recalculating');
+      });
 
 
       // Let shiny finish rendering
       cy
         .get('html')
-        .not('.shiny-busy')
+        .not('.shiny-busy');
 
     // Verify that data is filtered (selected subjets != dataset)
     // ------------------------------------
@@ -242,12 +243,10 @@ describe('app', () => {
       )
       // There
       .should('satisfy', ($el) => {
-        console.log($el[0])
-        console.log($el[0].innerText)
-        const result = /([0-9]+)\/([0-9]+)/.exec($el[0].innerText)
-        return result.length == 3 && result[1] != result[2]
-      })
-  })
+        const result = /([0-9]+)\/([0-9]+)/.exec($el[0].innerText);
+        return result.length == 3 && result[1] != result[2];
+      });
+  });
 
 
   //  Active tab
@@ -264,8 +263,8 @@ describe('app', () => {
               .wrap($el)
               .children()
               .should('have.length.gte', 1)
-          })
-      })
-  })
+          });
+      });
+  });
 
-})
+});
