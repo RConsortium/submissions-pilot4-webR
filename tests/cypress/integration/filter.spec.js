a85663a6490074a193030dbe9f6b3279e9791829
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-04-27 12:19:01 +0200

    code styling format

--- tests/cypress/integration/filter.spec.js
+++ tests/cypress/integration/filter.spec.js
@@ -1,4 +1,4 @@
-describe('Filter panes', () => {
+describe('Filter panes in KM tab', () => {
   // Rhino applications have an 'app' prefix
   const nsPrefix = 'app-';
   // Support function to help generating namespaces
@@ -6,12 +6,18 @@ describe('Filter panes', () => {
   // Support function to help generating teal namespaces on main UI
   const nsTeal = (id) => ns(`teal_wrapper-teal-main_ui-${id}`);
 
+  afterEach(() => {
+    cy.get('@active_tab').within(() => {
+      cy.get('.shiny-bound-output:visible').each(($el) => {
+        cy.wrap($el).children().should('have.length.gte', 1);
+      });
+    });
+  });
+
   beforeEach(() => {
     cy.visit('/');
 
-    cy
-      .get('html')
-      .not('.shiny-busy');
+    cy.get('html').not('.shiny-busy');
 
     // Find the tab and click to navigate
     cy
@@ -19,14 +25,9 @@ describe('Filter panes', () => {
       .contains('KM plot for TTDE')
       .as('navPills');
 
-    cy
-      .get('@navPills')
-      .click();
+    cy.get('@navPills').click();
 
-    cy
-      .get('@navPills')
-      .invoke('attr', 'href')
-      .as('hrefTab');
+    cy.get('@navPills').invoke('attr', 'href').as('hrefTab');
 
     // Make sure the correct tab is selected
     cy.get('@hrefTab').then((hrefTab) => {
@@ -40,14 +41,10 @@ describe('Filter panes', () => {
         .as('active_tab');
 
       // Define an alias that references the active filters
-      cy
-        .get(nsTeal('filter_panel-filters_overview'))
-        .as('filter_summary');
+      cy.get(nsTeal('filter_panel-filters_overview')).as('filter_summary');
 
       // Define an alias that references the active variables
-      cy
-        .get(nsTeal('filter_panel-filter_active_vars'))
-        .as('filter_variables');
+      cy.get(nsTeal('filter_panel-filter_active_vars')).as('filter_variables');
 
       // Define an alias that references the section that adds new variables to
       //  the filters
@@ -58,37 +55,44 @@ describe('Filter panes', () => {
 
       // Make sure that html element does not have a class that indicates
       // that shiny is busy
-      cy
-        .get('html')
-        .not('.shiny-busy');
+      cy.get('html').not('.shiny-busy');
     });
+  });
+
+  // Hamburger works hidding / showing
+  // ######################################
+
+  it('Hamburger menu shows by defaults and hides/shows when clicked', () => {
+    cy.get('#filter_hamburger').as('hamburger-menu');
+
+    // By default, the filter pane should be visible
+    cy.get('@filter_summary').should('be.visible');
+
+    // Hides the filter pane when clicking the button
+    cy.get('@hamburger-menu').click();
+    cy.get('@filter_summary').should('not.be.visible');
+
+    // Shows the filter pane when clicking the button again
+    cy.get('@hamburger-menu').click();
+    cy.get('@filter_summary').should('be.visible');
   });
 
   //  Filter summary
   // ######################################
 
   it('Filter Summary has non-zero Observations', () => {
-    cy
-      .get('@filter_summary')
-      .within(() => {
-        cy
-          .get('.shiny-bound-output')
-          .each(($el) => {
-            cy
-              .wrap($el)
-              .children()
-              .should('have.length.gte', 1);
-          });
+    cy.get('@filter_summary').within(() => {
+      cy.get('.shiny-bound-output').each(($el) => {
+        cy.wrap($el).children().should('have.length.gte', 1);
       });
+    });
   });
 
   //  Filter variables
   // ######################################
 
   it('Filter variables has shiny content rendered', () => {
-    cy
-      .get('@filter_variables')
-      .within(() => {
+    cy.get('@filter_variables').within(() => {
         cy
           .get(nsTeal('filter_panel-filter_active_vars_contents'))
           .should('have.length.gte', 1);
@@ -121,18 +125,11 @@ describe('Filter panes', () => {
   it('Add filter variables has shiny content rendered', {
     defaultCommandTimeout: 10000,
   }, () => {
-    cy
-      .get('@add_filter')
-      .within(() => {
-        cy
-          .get('.shiny-input-container')
-          .each(($el) => {
-            cy
-              .wrap($el)
-              .children()
-              .should('have.length.gte', 1);
-          });
+    cy.get('@add_filter').within(() => {
+      cy.get('.shiny-input-container').each(($el) => {
+        cy.wrap($el).children().should('have.length.gte', 1);
       });
+    });
   });
 
   //  Add variables to filter
@@ -143,27 +140,23 @@ describe('Filter panes', () => {
 
     // Add Age filter (find and click on it)
     // ------------------------------------
-    cy
-      .get('@add_filter')
-      .within(() => {
-        cy
-          .get('.shiny-input-container:first')
-          .contains('Select variable to filter')
-          .should('be.visible')
-          .parent()
-          .as('filterButton');
+    cy.get('@add_filter').within(() => {
+      cy
+        .get('.shiny-input-container:first')
+        .contains('Select variable to filter')
+        .should('be.visible')
+        .parent()
+        .as('filterButton');
 
-        cy.get('@filterButton', { timeout: 15000 }).click('top');
+      cy.get('@filterButton', { timeout: 15000 }).click('top');
 
-        cy.get('.dropdown-menu.open li').contains('Age').as('age');
+      cy.get('.dropdown-menu.open li').contains('Age').as('age');
 
-        cy.get('@age', { timeout: 15000 }).click('top');
-      });
+      cy.get('@age', { timeout: 15000 }).click('top');
+    });
 
     // Let shiny finish rendering
-    cy
-      .get('html')
-      .not('.shiny-busy');
+    cy.get('html').not('.shiny-busy');
 
     // Test if application has non-zero subjects
 
@@ -173,71 +166,65 @@ describe('Filter panes', () => {
 
     // Monitor how many times summary is recalculated vias shiny:recalculated
     // event
-    cy
-      .get('@filter_summary')
-      .within(() => {
-        cy
-          .get('.shiny-bound-output')
-          .invoke('on', 'shiny:recalculated', cy.stub().as('summary_change'));
-      });
+    cy.get('@filter_summary').within(() => {
+      cy
+        .get('.shiny-bound-output')
+        .invoke('on', 'shiny:recalculated', cy.stub().as('summary_change'));
+    });
 
     // Move slider via cypress2 mouse drag
     // ------------------------------------
-    cy
-      .get('@filter_variables')
-      .within(() => {
-        // Make sure that the overlay plot is rendered
-        cy
-          .get('.filterPlotOverlayRange .shiny-plot-output')
-          .children()
-          .should('have.length.gte', 1);
+    cy.get('@filter_variables').within(() => {
+      // Make sure that the overlay plot is rendered
+      cy
+        .get('.filterPlotOverlayRange .shiny-plot-output')
+        .children()
+        .should('have.length.gte', 1);
 
-        cy
-          .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating');
+      cy
+        .get('.filterPlotOverlayRange .shiny-plot-output')
+        .not('.recalculating');
 
-        // Move the handle 3x
-        //  note: this is necessary as developer wasn't able to define a single
-        //    long drag. This is open to improvement
-        cy.get('.irs-handle.from').as('irs-handle');
+      // Move the handle 3x
+      //  note: this is necessary as developer wasn't able to define a single
+      //    long drag. This is open to improvement
+      cy.get('.irs-handle.from').as('irs-handle');
 
-        const mousedownOpts = {
-          button: 0, which: 1, pageX: 600, pageY: 100,
-        };
-        const mousemoveOpts = {
-          clientX: 300, clientY: 400,
-        };
+      const mousedownOpts = {
+        button: 0, which: 1, pageX: 600, pageY: 100,
+      };
+      const mousemoveOpts = {
+        clientX: 300, clientY: 400,
+      };
 
-        cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
-        cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
-        cy.get('@irs-handle').trigger('mouseup', 'top');
+      cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
+      cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
+      cy.get('@irs-handle').trigger('mouseup', 'top');
 
-        cy
-          .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating');
-        // cy.get('@summary_change', { timeout: 20000 }).should('have.callCount', 1)
+      cy
+        .get('.filterPlotOverlayRange .shiny-plot-output')
+        .not('.recalculating');
+      // cy.get('@summary_change', { timeout: 20000 }).should('have.callCount', 1)
 
-        cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
-        cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
-        cy.get('@irs-handle').trigger('mouseup', 'top');
+      cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
+      cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
+      cy.get('@irs-handle').trigger('mouseup', 'top');
 
-        cy
-          .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating');
+      cy
+        .get('.filterPlotOverlayRange .shiny-plot-output')
+        .not('.recalculating');
 
-        cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
-        cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
-        cy.get('@irs-handle').trigger('mouseup', 'top');
+      cy.get('@irs-handle').trigger('mousedown', 'top', mousedownOpts);
+      cy.get('@irs-handle').trigger('mousemove', 'top', mousemoveOpts);
+      cy.get('@irs-handle').trigger('mouseup', 'top');
 
-        cy
-          .get('.filterPlotOverlayRange .shiny-plot-output')
-          .not('.recalculating');
-      });
+      cy
+        .get('.filterPlotOverlayRange .shiny-plot-output')
+        .not('.recalculating');
+    });
 
     // Let shiny finish rendering
-    cy
-      .get('html')
-      .not('.shiny-busy');
+    cy.get('html').not('.shiny-busy');
 
     // Verify that data is filtered (selected subjets != dataset)
     // ------------------------------------
@@ -246,24 +233,6 @@ describe('Filter panes', () => {
       .should('satisfy', ($el) => {
         const result = /([0-9]+)\/([0-9]+)/.exec($el[0].innerText);
         return result.length === 3 && result[1] !== result[2];
-      });
-  });
-
-  //  Active tab
-  // ######################################
-
-  it('Active tab has shiny content rendered', () => {
-    cy
-      .get('@active_tab')
-      .within(() => {
-        cy
-          .get('.shiny-bound-output:visible')
-          .each(($el) => {
-            cy
-              .wrap($el)
-              .children()
-              .should('have.length.gte', 1);
-          });
       });
   });
 });
