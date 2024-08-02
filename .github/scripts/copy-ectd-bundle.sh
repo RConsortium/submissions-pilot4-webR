b1c2bdf88939c26031b82af6a3d7e0d7c540fb57
Eric Nantz <thercast@gmail.com>
2024-08-02 17:38:36 +0000

    update ectd GH action

--- .github/scripts/copy-ectd-bundle.sh
+++ .github/scripts/copy-ectd-bundle.sh
@@ -10,14 +10,38 @@ function l { # Log a message to the terminal.
 # ECTD file to copy from source repo
 ECTD_BUNDLE_DIR=ectd_bundle
 ECTD_BUNDLE_FILE=r4app.zip
+ADRG_SOURCE_DIR=adrg
+ADRG_SOURCE_FILE=adrg-quarto-pdf.pdf
+ADRG_DEST_FILE=adrg.pdf
+README_SOURCE_DIR=ectd_readme
+README_SOURCE_FILE=README.md
+README_DEST_FILE=README.md
 
-if [ -f "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" ]; then
-  echo "Copying ${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}"
-  if [ ! -f "$DESTINATION_DIR" ]; then
-    echo "Create new directory ${DESTINATION_DIR}"
-    mkdir -p "${DESTINATION_DIR}"
+if [ -f "${ADRG_SOURCE_DIR}/${ADRG_SOURCE_FILE}" ]; then
+  echo "Copying ${ADRG_SOURCE_DIR}/${ADRG_SOURCE_FILE}"
+  if [ ! -f "$ADRG_DESTINATION_DIR" ]; then
+    echo "Create new directory ${ADRG_DESTINATION_DIR}"
+    mkdir -p "${ADRG_DESTINATION_DIR}"
   fi
-  cp "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" "${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
+  cp "${ADRG_SOURCE_DIR}/${ADRG_SOURCE_FILE}" "${ADRG_DESTINATION_DIR}/${ADRG_DEST_FILE}"
 fi
 
-echo "ECTD Bundle file copied to ${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
+echo "ADRG copied to ${ADRG_DESTINATION_DIR}/${ADRG_DEST_FILE}"
+
+if [ -f "${README_SOURCE_DIR}/${README_SOURCE_FILE}" ]; then
+  echo "Copying ${README_SOURCE_DIR}/${README_SOURCE_FILE}"
+  cp "${README_SOURCE_DIR}/${README_SOURCE_FILE}" "${README_DEST_FILE}"
+fi
+
+echo "README copied to ${README_DEST_FILE}"
+
+# if [ -f "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" ]; then
+#   echo "Copying ${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}"
+#   if [ ! -f "$DESTINATION_DIR" ]; then
+#     echo "Create new directory ${DESTINATION_DIR}"
+#     mkdir -p "${DESTINATION_DIR}"
+#   fi
+#   cp "${ECTD_BUNDLE_DIR}/${ECTD_BUNDLE_FILE}" "${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
+# fi
+
+# echo "ECTD Bundle file copied to ${DESTINATION_DIR}/${ECTD_BUNDLE_FILE}"
